852caf64326f39ca5d47fbbc80fe972f021194ef
Leyre <romeroleyredev@gmail.com>
2023-03-26 12:31:55 +0200

    js board

--- js/game.js
+++ js/game.js
@@ -1,7 +1,3 @@
-const htmlCasillas = document.querySelectorAll(".col-board");
-const mapCasillas = [];
-let turn = true;
-
 const secondInfo = () => {
   return `
     <!--Estructura juego-->
@@ -24,27 +20,27 @@ const secondInfo = () => {
         </div>
         <div class="board">
           <div class="fila">
-            <div class="col-board">
+            <div id="0" class="col-board">
             </div>
-            <div class="col-board">
+            <div id="1" class="col-board">
             </div>
-            <div class="col-board">
-            </div>
-          </div>
-          <div class="fila">
-            <div class="col-board">
-            </div>
-            <div class="col-board">
-            </div>
-            <div class="col-board">
+            <div id="2" class="col-board">
             </div>
           </div>
           <div class="fila">
-            <div class="col-board">
+            <div id="3" class="col-board">
             </div>
-            <div class="col-board">
+            <div id="4" class="col-board">
             </div>
-            <div class="col-board">
+            <div id="5" class="col-board">
+            </div>
+          </div>
+          <div class="fila">
+            <div id="6" class="col-board">
+            </div>
+            <div id="7" class="col-board">
+            </div>
+            <div id="8" class="col-board">
             
             </div>
           </div>
@@ -69,36 +65,108 @@ const renderGame = () => {
   root.classList.add("my-container");
   root.innerHTML = secondInfo();
 };
-const clickGame = (x) => {
-  let element = x.currentTarget;
-  console.log(element);
-  if ((element.innerHTML = " ")) {
-    if (turn) {
-      element.innerHTML = `<img src="../images/moneda.png" class="moneda" alt="moneda" />`;
-      mapCasillas[element.id] = "y";
-    } else {
-      element.innerHTML = `<img src="../images/calavera.png" class="moneda" alt="moneda" />`;
-      mapCasillas[element.id] = "x";
-    }
-    turn = !turn;
-  }
-  console.log(mapCasillas);
-  checkPlay();
-};
 
-const checkPlay = (c) => {
-  if (c == 1) {
-    renderWinner();
-    winnerEvents();
-  } else {
-    renderWinner();
-    winnerEvents();
+const checkPlay = (playerTurn, winnerArray) => {
+  const checkWinner = () => {
+    if (playerTurn == "y") {
+      gameData.winner = gameData.player1.name;
+      renderWinner();
+      winnerEvents();
+    } else if (playerTurn == "x") {
+      gameData.winner = gameData.player2.name;
+      renderWinner();
+      winnerEvents();
+    }
+  };
+  if (
+    winnerArray[0] == playerTurn &&
+    winnerArray[1] == playerTurn &&
+    winnerArray[2] == playerTurn
+  ) {
+    checkWinner();
+  } else if (
+    winnerArray[3] == playerTurn &&
+    winnerArray[4] == playerTurn &&
+    winnerArray[5] == playerTurn
+  ) {
+    checkWinner();
+  } else if (
+    winnerArray[6] == playerTurn &&
+    winnerArray[7] == playerTurn &&
+    winnerArray[8] == playerTurn
+  ) {
+    checkWinner();
+  } else if (
+    winnerArray[0] == playerTurn &&
+    winnerArray[3] == playerTurn &&
+    winnerArray[6] == playerTurn
+  ) {
+    checkWinner();
+  } else if (
+    winnerArray[1] == playerTurn &&
+    winnerArray[4] == playerTurn &&
+    winnerArray[7] == playerTurn
+  ) {
+    checkWinner();
+  } else if (
+    winnerArray[2] == playerTurn &&
+    winnerArray[5] == playerTurn &&
+    winnerArray[8] == playerTurn
+  ) {
+    checkWinner();
+  } else if (
+    winnerArray[0] == playerTurn &&
+    winnerArray[4] == playerTurn &&
+    winnerArray[8] == playerTurn
+  ) {
+    checkWinner();
+  } else if (
+    winnerArray[2] == playerTurn &&
+    winnerArray[4] == playerTurn &&
+    winnerArray[6] == playerTurn
+  ) {
+    checkWinner();
+  } else if (
+    winnerArray[0] != undefined &&
+    winnerArray[1] != undefined &&
+    winnerArray[2] != undefined &&
+    winnerArray[3] != undefined &&
+    winnerArray[4] != undefined &&
+    winnerArray[5] != undefined &&
+    winnerArray[6] != undefined &&
+    winnerArray[7] != undefined &&
+    winnerArray[8] != undefined
+  ) {
+    renderDraw();
+    drawEvents();
   }
 };
 
 const gameEvents = () => {
+  const mapCasillas = [];
+  let turn = true;
   const casillas = document.querySelectorAll(".col-board");
+
+  const clickGame = (element) => {
+    console.log(element);
+    if ((element.innerHTML = " ")) {
+      if (turn) {
+        element.innerHTML = `<img src="../images/moneda.png" class="moneda" alt="moneda" />`;
+        mapCasillas[element.id] = "y";
+      } else {
+        element.innerHTML = `<img src="../images/calavera.png" class="moneda" alt="calavera" />`;
+        mapCasillas[element.id] = "x";
+      }
+      turn = !turn;
+      console.log(mapCasillas);
+      checkPlay(mapCasillas[element.id], mapCasillas);
+    }
+  };
+
   for (casilla of casillas) {
-    casilla.addEventListener("click", clickGame);
+    casilla.addEventListener("click", (event) => {
+      const boardSquare = event.target;
+      clickGame(boardSquare);
+    });
   }
 };
--- js/winner.js
+++ js/winner.js
@@ -2,7 +2,7 @@ const winnerData = () => {
   return `
     <div class="container-pl1">
         <div class="winner">
-          <h1 class="winner-title" id="position-pl1">PLAYER 1, YOU WIN!</h1>
+          <h1 class="winner-title" id="position-pl1">${gameData.winner} YOU WIN!</h1>
         </div>
         <div class="winner-text">
           <h1 class="winner-title" id="position-pl2">Welcome on board.</h1>
@@ -25,7 +25,39 @@ const renderWinner = () => {
 const winnerEvents = () => {
   document.getElementById("btn-restart").addEventListener("click", () => {
     root.classList.remove("my-container2");
-    renderIndexInfo();
-    indexEvents();
+    renderMainPageHTML();
+    addListeners();
   });
+};
+
+const drawEvents = () => {
+  document.getElementById("btn-restart").addEventListener("click", () => {
+    root.classList.remove("my-container2");
+    renderMainPageHTML();
+    addListeners();
+  });
+};
+
+const drawHtml = () => {
+  return `
+    <div class="container-pl1">
+        <div class="winner">
+          <h1 class="winner-title" id="position-pl1">IT LOOKS LIKE A TIE...</h1>
+        </div>
+        <div class="winner-text">
+          <h1 class="winner-title" id="position-pl2">Try again.</h1>
+          <p></p>
+        </div>
+        <div class="but-2">
+         
+            <button id="btn-restart" class="but2-style">START AGAIN</button>
+        </div>
+      </div>
+    `;
+};
+
+const renderDraw = () => {
+  root.classList.remove("my-container");
+  root.classList.add("my-container2");
+  root.innerHTML = winnerData();
 };
